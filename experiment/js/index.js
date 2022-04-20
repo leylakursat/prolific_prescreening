f0c072c320b7c2f1043220310b65219d0b88d052
leylakursat <leylakursat@gmail.com>
2022-04-19 17:01:26 -0700

    first commit

---
+++ experiment/js/index.js
@@ -0,0 +1,161 @@
+function make_slides(f) {
+  var   slides = {};
+
+  slides.bot = slide({
+    name : "bot",
+    start: function() {
+      $('.err1').hide();
+      $('.err2').hide();
+      $('.disq').hide();
+      exp.speaker = _.shuffle(["James", "John", "Robert", "Michael", "William", "David", "Richard", "Joseph", "Thomas", "Charles"])[0];
+      exp.listener = _.shuffle(["Mary", "Patricia", "Jennifer", "Linda", "Elizabeth", "Barbara", "Susan", "Jessica", "Sarah", "Margaret"])[0];
+      exp.lives = 0;
+      var story = exp.speaker + ' says to ' + exp.listener + ': "It\'s a beautiful day, isn\'t it?"'
+      var question = 'Who does ' + exp.speaker + ' talk to?';
+      document.getElementById("s").innerHTML = story;
+      document.getElementById("q").innerHTML = question;
+    },
+    button : function() {
+      exp.text_input = document.getElementById("text_box").value;
+      var lower = exp.listener.toLowerCase();
+      var upper = exp.listener.toUpperCase();
+
+      if ((exp.lives < 3) && ((exp.text_input == exp.listener)|(exp.text_input == lower) | (exp.text_input== upper))){
+        exp.data_trials.push({
+          "slide_number": exp.phase,
+          "trial_type" : "bot_check",
+          "image" : exp.listener,
+          "response" : [0,exp.text_input]
+        });
+        exp.go();
+      }
+      else {
+        exp.data_trials.push({
+          "slide_number": exp.phase,
+          "trial_type" : "bot_check",
+          "image" : exp.listener,
+          "response" : [0,exp.text_input]
+        });
+        if (exp.lives == 0){
+          $('.err1').show();
+        }if (exp.lives == 1){
+          $('.err1').hide();
+          $('.err2').show();
+        }if (exp.lives == 2){
+          $('.err2').hide();
+          $('.disq').show();
+          $('.button').hide();
+        }
+        exp.lives++;
+      } 
+    },
+  });
+
+  slides.i0 = slide({
+     name : "i0",
+     start: function() {
+      exp.startT = Date.now();
+     }
+  });
+
+  slides.description = slide({
+    name: "description",
+    start: function() {
+      $(".err").hide();
+    },
+    button : function() {
+        exp.go();
+    }
+  });
+
+  slides.questions = slide({
+    name: "questions",
+    start: function() {
+      $(".err").hide();
+    },
+    button : function() {
+      var qual = $('input[name="qualify"]:checked').val();
+      var agr = $('input[name="agree"]:checked').val()
+      //if (e.preventDefault) e.preventDefault(); // I don't know what this means.
+      exp.data_trials.push({
+        qualify: qual,
+        agree: agr,
+        email: $("#email").val(),
+      });
+      if (qual=="Yes" & agr=="Yes") {
+        console.log("yes")
+        exp.go();
+      } else {
+        console.log("no")
+        exp.go("thanks");
+      }
+      //exp.go(); //use exp.go() if and only if there is no "present" data.
+    }
+  });
+
+  slides.info = slide({
+    name: "info",
+    start: function() {
+    },
+    button : function() {
+        exp.go();
+    }
+  });
+
+  slides.thanks = slide({
+    name : "thanks",
+    start : function() {  
+      exp.data= {
+          "trials" : exp.data_trials,
+          "catch_trials" : exp.catch_trials,
+          "system" : exp.system,
+          //"condition" : exp.condition,
+          //"subject_information" : exp.subj_data,
+          "time_in_minutes" : (Date.now() - exp.startT)/60000
+      };
+      setTimeout(function() {turk.submit(exp.data);}, 1000);
+    }
+  });
+
+  return slides;
+}
+
+
+
+/// init ///
+function init() {
+  exp.trials = [];
+  exp.catch_trials = [];
+  //exp.condition = _.sample(["condition 1", "condition 2"]); //can randomize between subject conditions here
+  exp.system = {
+      Browser : BrowserDetect.browser,
+      OS : BrowserDetect.OS,
+      screenH: screen.height,
+      screenUH: exp.height,
+      screenW: screen.width,
+      screenUW: exp.width
+    };
+  //blocks of the experiment:
+  exp.structure=["bot", "i0", "description", "questions", "info", 'thanks'];
+
+  exp.data_trials = [];
+  //make corresponding slides:
+  exp.slides = make_slides(exp);
+
+  exp.nQs = utils.get_exp_length(); //this does not work if there are stacks of stims (but does work for an experiment with this structure)
+                    //relies on structure and slides being defined
+
+  $('.slide').hide(); //hide everything
+
+  //make sure turkers have accepted HIT (or you're not in mturk)
+  $("#start_button").click(function() {
+    if (turk.previewMode) {
+      $("#mustaccept").show();
+    } else {
+      $("#start_button").click(function() {$("#mustaccept").show();});
+      exp.go();
+    }
+  });
+
+  exp.go(); //show first slide
+}
